aa01dcfdd2d216c18ab954eb2eb0346b636e19b5
Cassio Azevedo <cassioviller@gmail.com>
2026-06-01 01:07:04 +0000

    chore: fix cross-origin preview, post-merge script e gitignore

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const devOrigins: string[] = [];
+if (process.env.REPLIT_DEV_DOMAIN) {
+  devOrigins.push(process.env.REPLIT_DEV_DOMAIN);
+}
+if (process.env.REPLIT_DOMAINS) {
+  devOrigins.push(...process.env.REPLIT_DOMAINS.split(","));
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  ...(devOrigins.length > 0 && { allowedDevOrigins: devOrigins }),
 };
 
 export default nextConfig;
